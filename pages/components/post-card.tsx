57d8e1f03ecb07d09ad869c89a188ea06c18caa4
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-15 21:53:26 -0800

    fix styling

--- pages/components/post-card.tsx
+++ pages/components/post-card.tsx
@@ -9,13 +9,13 @@ import moment from "moment"
 
 export default function PostCard({post}: {post: any}) {
   return (
-    <div className="flex border-[1px] border-gray-300 w-fit rounded-lg bg-slate-50 hover:border-black">
-      <div className="arrows-area w-fit flex flex-col items-center p-1 pt-3">
+    <div className="flex border-[1px] border-gray-300 w-[600px] rounded-lg bg-slate-50 hover:border-black">
+      <div className="arrows-area w-fit flex flex-col items-center p-2 pt-3">
         <ArrowUpSquare />
         <p className="font-semibold text-xs">{post.totalLikes}</p>
         <ArrowDownSquare />
       </div>
-      <div className="main-content bg-white rounded-tr-lg rounded-br-lg">
+      <div className="main-content bg-white w-full rounded-tr-lg rounded-br-lg">
         <div className="flex justify-between items-center mx-4 my-2">
           <div className="flex gap-2">
             <div className="font-bold text-xs">r/formuladank</div>
@@ -27,9 +27,9 @@ export default function PostCard({post}: {post: any}) {
           {/* <h1 className="text-xl font-semibold ml-2 mb-2">These guys take zero Ws, they are Illiams now</h1> */}
           <h1 className="text-xl font-semibold ml-2 mb-2">{post.title}</h1>
           <h2 className="text-sm font-semibold text-gray-600 ml-2 mb-2">{post.description}</h2>
-          <div className="photo if theres a photo">
+          {/* <div className="photo if theres a photo">
             <img className="max-h-[480px]" src="https://i.redd.it/i277m1nkai5a1.png" alt="" />
-          </div>
+          </div> */}
           <div className="flex gap-4 p-2 items-center">
             <div className="flex gap-2 items-center hover:bg-gray-200">
               <ChatSvg />
