87db050a3d884f9d5bec11fbb8f803bfdc23dc20
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-18 17:37:50 -0800

    remove migration fails, add migrations

--- prisma/migrations/20221219013558_add_username/migration.sql
+++ prisma/migrations/20221219013558_add_username/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "username" TEXT NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "username" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
