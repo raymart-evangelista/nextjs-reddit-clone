fe05d7a59a827ddf41e749a3ad88d4c3b4ba0d16
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-21 10:19:15 -0800

    add getServerSideProps for posts in each subreddit

--- pages/r/[subredditName]/index.tsx
+++ pages/r/[subredditName]/index.tsx
@@ -1,6 +1,27 @@
+import { GetServerSideProps } from "next"
 import { useRouter } from "next/router"
+import { prisma } from "../../../server/db/client"
 
-export default function Subreddit() {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      subreddit: String(params?.subredditName)
+    },
+    include: {
+      author: {
+        select: { username: true }
+      }
+    }
+  })
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts))
+    }
+  }
+}
+
+export default function Subreddit(props: any) {
+  console.log(props)
   const router = useRouter()
   const subredditName = router.query.subredditName
   return (
