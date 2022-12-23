e1c415d031b3ac341abd0119ef0ca6099b9d325e
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-23 11:35:00 -0800

    preseed

---
+++ prisma/seed.ts
@@ -0,0 +1,11 @@
+// import { prisma } from "../server/db/client";
+
+// async function main() {
+//   const post1 = await prisma.post.upsert({
+//     create: {
+//       title: 'Seeded Post 1',
+//       description: 'this post was seeded',
+      
+//     }
+//   })
+// }
